9b83969f9e6d3559e2400cb718e5cb7485a34c57
MainUser <MainUser@DESKTOP-CSCP8IK>
2024-10-01 18:39:26 +1000

    Добавьте файлы проекта.

---
+++ RemainderOfDivision/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RemainderOfDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
