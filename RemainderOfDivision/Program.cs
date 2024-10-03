edce066fe150b3dd774ab79e2f4849d2960238f3
Vadim Lisovenko <lisovenkovn46@gmail.com>
2024-10-03 21:33:01 +1000

    я сделяль

--- RemainderOfDivision/Program.cs
+++ RemainderOfDivision/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace RemainderOfDivision
+﻿using System.Globalization;
+
+namespace RemainderOfDivision
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                string InputA, InputB;
+                int a, b, quotient, remainder;
+
+                Console.WriteLine("Введите числитель:");
+                InputA = Console.ReadLine();
+
+                try
+                {
+                    a = int.Parse(InputA);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Некорректный ввод: {InputA}. Перевведите");
+                    continue;
+                } 
+
+                Console.WriteLine("Введите знаменатель:");
+                InputB = Console.ReadLine();
+                try
+                {
+                    b = int.Parse(InputB);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Некорректный ввод: {InputB}. Перевведите");
+                    continue;
+                }
+
+                if (b == 0)
+                {
+                    Console.WriteLine("Некорректный ввод. Знаменатель не может быть равен 0");
+                    continue;
+                }
+
+                quotient = a / b;
+                remainder = a % b;
+                Console.WriteLine($"Результат деления: {quotient}, Остаток от деления: {remainder}");
+                Console.WriteLine("---------------------------------------------------------------");
+            }
         }
     }
 }
